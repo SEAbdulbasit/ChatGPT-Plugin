b22e42d6a090aa6a511f68aade018c414b8bf29a
GitHub Action <action@github.com>
2023-03-24 04:29:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/seabdulbasit/chatgptplugin/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/seabdulbasit/chatgptplugin/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.seabdulbasit.chatgptplugin.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
