3a5d3f73bc78d6245071266071e96d61d0a70b26
Abdul Basit <abdulbasit5361234@gmail.com>
2023-03-24 22:22:22 +0500

    Add webview for ChatGPT

--- src/main/kotlin/com/github/seabdulbasit/chatgptplugin/toolWindow/ChatGPTWidowFactory.kt
+++ src/main/kotlin/com/github/seabdulbasit/chatgptplugin/toolWindow/ChatGPTWidowFactory.kt
@@ -1,19 +1,15 @@
 package com.github.seabdulbasit.chatgptplugin.toolWindow
 
-import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import com.github.seabdulbasit.chatgptplugin.MyBundle
-import com.github.seabdulbasit.chatgptplugin.services.MyProjectService
-import javax.swing.JButton
+import com.intellij.ui.jcef.JBCefBrowser
 
 
-class MyToolWindowFactory : ToolWindowFactory {
+class ChatGPTWidowFactory : ToolWindowFactory {
 
     init {
         thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
@@ -31,17 +27,11 @@ class MyToolWindowFactory : ToolWindowFactory {
 
     class MyToolWindow(toolWindow: ToolWindow) {
 
-        private val service = toolWindow.project.service<MyProjectService>()
 
         fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel(MyBundle.message("randomLabel", "?"))
-
-            add(label)
-            add(JButton(MyBundle.message("shuffle")).apply {
-                addActionListener {
-                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
-                }
-            })
+            val webView = JBCefBrowser()
+            webView.loadURL("https://chat.openai.com/")
+            add(webView.component)
         }
     }
 }
